903027733dfb898ff5d207053703b0729e330e4c
Spandan <rsehgal746@gmail.com>
2022-05-21 16:21:42 +0530

    updated footer,header and added about and projects

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,20 @@ import '../styles/globals.css'
 import  Head  from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import About from '../components/About'
+import Projects from '../components/Projects'
 
 function MyApp() {
   return (
-    <>
+<>    
     <Head>
       <title>SpandyBoss&apos;s Portfolio</title>
     </Head>
 
    <Header/>
-    <Footer/>
+       <About/>
+        <Projects/>
+  <Footer/>
 
 </>
   )
